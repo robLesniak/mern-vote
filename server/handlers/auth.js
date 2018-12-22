0609e7fb87043f07d57e4d246e4182756cfaf157
Robert <r.lesiniak@gmail.com>
2018-12-22 18:35:10 +0100

    JWT authorization added for login and register

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -4,8 +4,11 @@ const db = require('../models');
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
-    const {id, username} = user;
-    res.status(201).json({id, username});
+    const { id, username } = user;
+
+    const token = jwt.sign({ id, username }, process.env.SECRET);
+
+    res.status(201).json({ id, username, token });
   } catch (err) {
     if (err.code === 11000) {
       err.message = 'Sorry, that username is already taken';
@@ -17,13 +20,16 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const user = await db.User.findOne({ username: req.body.username });
-    const {id, username} = user;
+    const { id, username } = user;
     const valid = await user.comparePassword(req.body.password);
 
-    if (valid){
+    if (valid) {
+      const token = jwt.sign({ id, username }, process.env.SECRET);
+
       res.json({
         id,
-        username
+        username,
+        token
       })
     } else {
       throw new Error();
--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(DATABASE, {
+mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true
 });
 
