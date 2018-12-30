e85b9a35ab8fc0870bcdca8a93864d1dd6574a5a
Robert <r.lesiniak@gmail.com>
2018-12-30 17:06:51 +0100

    added lifecycle method for login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import api from '../services/api'
 
-const App = () => {
+export default class App extends Component {
+
+  async componentDidMount(){
+    const result = await api.call('post', 'auth/login', {
+      username: 'username',
+      password: 'password'
+    }); 
+    console.log(result)
+  }
+
+  render() {
     return (
-        <div>asd</div>
+      <div>
+        asd
+      </div>
     )
+  }
 }
-
-export default App;
