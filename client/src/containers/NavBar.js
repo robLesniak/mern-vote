b1e579624580821f06612c2ce316a4c74a5da02c
Robert <r.lesiniak@gmail.com>
2019-01-12 15:44:50 +0100

    Added login/logout into front-end

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { logout } from '../store/actions'
 
-const NavBar = () => (
-  <div>NavBar</div>
+const NavBar = ({ auth, logout }) => (
+  <div>
+    <ul>
+      <li><Link to='/login'>Login</Link></li>
+      <li><Link to='/register'>Register</Link></li>
+      <li>
+        <a onClick={logout}>Logout</a>
+      </li>
+    </ul>
+    {auth.isAuthenticated && (<p>Logged in as {auth.user.username}</p>)}
+  </div>
 )
 
-export default NavBar;
+export default connect(
+  store => ({ auth: store.auth }),
+  { logout })
+  (NavBar);
